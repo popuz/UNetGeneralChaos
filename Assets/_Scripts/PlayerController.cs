ea6599a3d70a9e84e67d23f6d9a12cc644f7700f
Vitaly Popuzin <popuzin@gmail.com>
2018-12-14 17:35:14 +0300

    Учитывая предыдущие изменения переписал основной контрол

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -1,40 +1,31 @@
 ﻿using UnityEngine;
+using UnityEngine.AI;
 
-[RequireComponent(typeof(UnitMovement))]
+[RequireComponent(typeof(NavMeshAgent))]
 public class PlayerController : MonoBehaviour
-{    
+{
+    private IPlayerInput _input = new ClickToMoveInputScheme();
+    private IUnitMovement _unitMover = new ClickToMoveUnitMover();
+
     [SerializeField, Tooltip("слой взаимодействия перемещения")]
     private LayerMask _movementMask;
+    public LayerMask MovementMask => _movementMask;
 
-    private Camera _cam;
-    private UnitMovement _unitMovement;
+    private UnitAnimation _animCtrl = new UnitAnimation();
 
     void Awake()
     {
-        _cam = Camera.main;
-        _unitMovement = GetComponent<UnitMovement>();
-
-        _cam.GetComponent<CameraController>().Target = transform; /// установка цели слежения для камеры
+        _unitMover.Init(_input, this);
+        _animCtrl.Init(GetComponentInChildren<Animator>(), GetComponent<NavMeshAgent>());
     }
 
-    void Update()
-    {       
-        if (Input.GetMouseButtonDown(0))
-        {
-            Ray ray = _cam.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
+    private void Update()
+    {
+        _input.ReadInput();
+    }
 
-            if (Physics.Raycast(ray, out hit, 100f, _movementMask))
-                _unitMovement.MoveToPoint(hit.point);
-        }
-
-        //if (Input.GetMouseButtonDown(1))
-        //{
-        //    Ray ray = _cam.ScreenPointToRay(Input.mousePosition);
-        //    RaycastHit hit;
-
-        //    if (Physics.Raycast(ray, out hit, 100f, _movementMask))
-        //    { }
-        //}
+    private void FixedUpdate()
+    {
+        _animCtrl.Tick();
     }
 }
